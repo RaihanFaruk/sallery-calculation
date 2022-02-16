e94cae9309bc307d32a7c38e717d0950a8d9b453
RaihanFaruk <raihanuzamanfaruk@gmail.com>
2022-02-17 02:19:33 +0600

    css file added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
-// calculator button
-document.getElementById('calculator').addEventListener('click', function(){
-    const foodCost = document.getElementById('')
-})
+// // calculator button
+// document.getElementById('calculator').addEventListener('click', function(){
+//     const userIncome = document.getElementById('userIncome').value ;
+//     // all spend sum
+//     const foodCost = document.getElementById('foodCost').value ;
+//     const foodCost = document.getElementById('rentCost').value ;
+//     const foodCost = document.getElementById('clothCost').value ;
+//     const foodCost = document.getElementById('totalSpend') ;
+//     const totalSpend1 = parseFloat(rentCost) +
+    
+//     // const totalCost =
+
+// })
